5f7a2cba66b28f716a4828fc470a49d8e08de529
hheo <hheo@cozmoworks.com>
2018-01-24 12:55:23 +0900

    [수정] PHP 버전 변경 지원 5.6 7.0 7.1 7.2

--- lemp-stack.sh
+++ lemp-stack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 DOMAIN="domain.com"
 DBROOTPASS="dbrootpass"
+PHPVERSION="7.2" ## PHP 5.6 7.0 7.1 7.2 지원
 PHPMYADMIN_VERSION="4.7.7"
 PHPMYADMIN_BLOWFISH=")Aje8s~9VE|JyV8s7MF0Zw|DReayVcpU^"
 PHPMYADMIN_DIRECTORY="phpMyAdmin"
@@ -10,18 +11,20 @@ DOCUMENT_ROOT="/home/${USERID}/app/public"
 EMAIL="userid@domain.com"
 
 ## 타임존, 언어셋, 호스트네임, 저장소수정 패키지 업데이트
-locale-gen ko_KR.UTF-8
-echo "Asia/Seoul" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
-
-sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost ${DOMAIN}/" /etc/hosts
-
 sed -i 's/kr.archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 apt -y autoremove
+
+locale-gen ko_KR.UTF-8
+echo "Asia/Seoul" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+echo "${DOMAIN}" > /etc/hostname
+hostname -F /etc/hostname
+sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost ${DOMAIN}/" /etc/hosts
 
 ## sendmail, vsftpd, unzip 설치
 apt-get -y install sendmail
@@ -59,16 +62,16 @@ service nginx restart
 ## php 설치
 ## libargon2-0 libsodium23 php-common php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-readline
 
-apt-get -y install php7.2-fpm php7.2-intl php7.2-gd php7.2-curl php7.2-mbstring php7.2-xml
-sed -i 's/;emergency_restart_threshold = 0/emergency_restart_threshold = 10/' /etc/php/7.2/fpm/php-fpm.conf
-sed -i 's/;emergency_restart_interval = 0/emergency_restart_interval = 1m/' /etc/php/7.2/fpm/php-fpm.conf
-service php7.2-fpm restart
+apt-get -y install php$PHPVERSION-fpm php$PHPVERSION-intl php$PHPVERSION-gd php$PHPVERSION-curl php$PHPVERSION-mbstring php$PHPVERSION-xml
+sed -i 's/;emergency_restart_threshold = 0/emergency_restart_threshold = 10/' /etc/php/$PHPVERSION/fpm/php-fpm.conf
+sed -i 's/;emergency_restart_interval = 0/emergency_restart_interval = 1m/' /etc/php/$PHPVERSIONfpm/php-fpm.conf
+service php$PHPVERSION-fpm restart
 
 ## mariadb 설치
 debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password ${DBROOTPASS}"
 debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password ${DBROOTPASS}"
 apt-get -y install mariadb-server-10.2 mariadb-client-10.2
-apt-get -y install php7.2-mysql
+apt-get -y install php$PHPVERSION-mysql
 
 echo "[client]
 default-character-set=utf8mb4
@@ -96,13 +99,120 @@ service mysql restart
 
 ## memcached 캐시서버 설치
 apt-get -y install memcached
-apt-get -y install php7.2-memcached
+apt-get -y install php$PHPVERSION-memcached
 
 
 ## nginx 기본 설정 변경
-cat ~/lemp-stack/nginx/nginx.conf > /etc/nginx/nginx.conf
-cat ~/lemp-stack/nginx/default.conf > /etc/nginx/conf.d/default.conf
-cat ~/lemp-stack/nginx/fastcgi_params > /etc/nginx/fastcgi_params
+echo "fastcgi_param   QUERY_STRING            \$query_string;
+fastcgi_param   REQUEST_METHOD          \$request_method;
+fastcgi_param   CONTENT_TYPE            \$content_type;
+fastcgi_param   CONTENT_LENGTH          \$content_length;
+
+fastcgi_param   SCRIPT_FILENAME         \$document_root\$fastcgi_script_name;
+fastcgi_param   SCRIPT_NAME             \$fastcgi_script_name;
+fastcgi_param   PATH_INFO               \$fastcgi_path_info;
+fastcgi_param   PATH_TRANSLATED         \$document_root\$fastcgi_path_info;
+fastcgi_param   REQUEST_URI             \$request_uri;
+fastcgi_param   DOCUMENT_URI            \$document_uri;
+fastcgi_param   DOCUMENT_ROOT           \$document_root;
+fastcgi_param   SERVER_PROTOCOL         \$server_protocol;
+
+fastcgi_param   GATEWAY_INTERFACE       CGI/1.1;
+fastcgi_param   SERVER_SOFTWARE         nginx/\$nginx_version;
+
+fastcgi_param   REMOTE_ADDR             \$remote_addr;
+fastcgi_param   REMOTE_PORT             \$remote_port;
+fastcgi_param   SERVER_ADDR             \$server_addr;
+fastcgi_param   SERVER_PORT             \$server_port;
+fastcgi_param   SERVER_NAME             \$server_name;
+
+fastcgi_param   HTTPS                   \$https;
+
+# PHP only, required if PHP was built with --enable-force-cgi-redirect
+fastcgi_param   REDIRECT_STATUS         200;
+" > /etc/nginx/fastcgi_params
+
+echo "user  www-data;
+
+### 1 코어당 1 프로세스 ###
+worker_processes  1; 
+
+pid        /var/run/nginx.pid;
+error_log  /var/log/nginx/error.log error;
+
+events {
+    ### 동시에 몇 접속까지 허용할지 ulimit -n 을 통해서 측정 ###
+    worker_connections  1024;
+}
+
+### Nginx 버전 표기 ###
+server_tokens off;
+
+### 보안 적용 ###
+add_header X-Frame-Options SAMEORIGIN;
+add_header X-Content-Type-Options nosniff;
+add_header X-XSS-Protection '1;mode=block';
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+    access_log off;
+
+    ### buffers ###
+    client_body_buffer_size     16k;
+    client_header_buffer_size   1k;
+    client_max_body_size        0;
+    large_client_header_buffers 2 1k;
+
+    ### timeouts ###
+    client_body_timeout     12;
+    client_header_timeout   12;
+    keepalive_timeout       15;
+    send_timeout            10;
+
+    ### compression ###
+    gzip                    on;
+    gzip_comp_level         2;
+    gzip_min_length         1000;
+    gzip_proxied            expired no-cache no-store private auth;
+    gzip_types              text/plain text/css text/x-component
+                            text/xml application/xml application/xhtml+xml application/json
+                            image/x-icon image/bmp image/svg+xml application/atom+xml
+                            text/javascript application/javascript application/x-javascript
+                            application/pdf application/postscript
+                            application/rtf application/msword
+                            application/vnd.ms-powerpoint application/vnd.ms-excel
+                            application/vnd.ms-fontobject application/vnd.wap.wml
+                            application/x-font-ttf application/x-font-opentype;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+" > /etc/nginx/nginx.conf
+
+echo "server {
+    listen 80 default_server;
+    server_name localhost;
+    root /usr/share/nginx/html;
+
+    location / {
+        index  index.php index.html;
+    }
+
+    location ~ [^/]\.php(/|$) {
+        fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+        if (!-f \$document_root\$fastcgi_script_name) {
+            return 404;
+        }
+
+        fastcgi_pass unix:/run/php/php${PHPVERSION}-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_buffers 64 16k;
+
+        include fastcgi_params;
+    }
+}
+" > /etc/nginx/conf.d/default.conf
+
 service nginx restart
 
 ## phpmyadmin 설치
@@ -161,8 +271,8 @@ php_admin_value[session.save_path] = '127.0.0.1:11211'
 php_admin_value[realpath_cache_size] = 64k
 
 php_admin_value[short_open_tag] = On
-" > /etc/php/7.2/fpm/pool.d/$USERID.conf
-service php7.2-fpm restart
+" > /etc/php/$PHPVERSION/fpm/pool.d/$USERID.conf
+service php$PHPVERSION-fpm restart
 
 ## 사용자 nginx 설정
 echo "server {
@@ -170,7 +280,7 @@ echo "server {
     server_name ${DOMAIN};
     root        ${DOCUMENT_ROOT};
 
-    access_log /home/${USERID}/log/access.log;
+    access_log /home/${USERID}/log/access.log main;
     error_log  /home/${USERID}/log/error.log warn;
 
     location / {
@@ -224,8 +334,14 @@ echo "server {
     # Add PHP handler
     location ~ [^/]\.php(/|$) {
         fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+        if (!-f \$document_root\$fastcgi_script_name) {
+            return 404;
+        }
+
         fastcgi_pass unix:/run/php/${USERID}.sock;
         fastcgi_index index.php;
+        fastcgi_buffers 64 16k;
+
         include fastcgi_params;
     }
 }
@@ -259,7 +375,7 @@ server {
     server_name  ${DOMAIN};
     root   ${DOCUMENT_ROOT};
 
-    access_log /home/${USERID}/log/access.log;
+    access_log /home/${USERID}/log/access.log main;
     error_log  /home/${USERID}/log/error.log warn;
 
     ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;
@@ -270,7 +386,7 @@ server {
     add_header Strict-Transport-Security 'max-age=63072000';
     add_header X-Frame-Options SAMEORIGIN;
     add_header X-Content-Type-Options nosniff;
-    add_header X-XSS-Protection '1; mode=block';
+    add_header X-XSS-Protection '1;mode=block';
 
     # Set caches, protocols, and accepted ciphers. This config will merit an A+ SSL Labs score.
     ssl_session_cache shared:SSL:20m;
@@ -330,8 +446,14 @@ server {
     # Add PHP handler
     location ~ [^/]\.php(/|$) {
         fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+        if (!-f \$document_root\$fastcgi_script_name) {
+            return 404;
+        }
+
         fastcgi_pass unix:/run/php/${USERID}.sock;
         fastcgi_index index.php;
+        fastcgi_buffers 64 16k;
+
         include fastcgi_params;
     }
 }
@@ -346,8 +468,8 @@ mkdir /backup
 chmod 700 /backup
 cd ~
 echo "#!/bin/bash
-tar -czpf /backup/${USERID}.`date +%Y%m%d%H%M%S`.tgz /home/${USERID} 1>/dev/null 2>/dev/null
-mysqldump --extended-insert=FALSE -u${USERID} -p${USERPW} ${USERID} > /backup/${USERID}.`date +%Y%m%d%H%M%S`.sql
+tar -czpf /backup/${USERID}.\`date +%Y%m%d%H%M%S\`.tgz /home/${USERID} 1>/dev/null 2>/dev/null
+mysqldump --extended-insert=FALSE -u${USERID} -p${USERPW} ${USERID} > /backup/${USERID}.\`date +%Y%m%d%H%M%S\`.sql
 find /backup/ -type f -mtime +10 | sort | xargs rm -f
 " > backup.sh
 chmod 700 backup.sh
